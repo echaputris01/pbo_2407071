ae2982e996104010a3e926dbaf1a79468f26673a
echaputris01 <echaputris01@gmail.com>
2026-02-20 11:00:50 +0700

    update terbaru main.java

--- latihan_2407071/src/latihan_pbo/latihan_1/main.java
+++ latihan_2407071/src/latihan_pbo/latihan_1/main.java
@@ -11,6 +11,29 @@ class cafe {
         System.out.println("Alamatnya di : " + alamat);
         System.out.println("===============================");
     }
+
+    //method
+    void buka(){
+        System.out.println("===============================");
+        System.out.println("Cafe suka buka");
+        
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 }
 public class main { 
     public static void main(String[] args) {
@@ -18,6 +41,7 @@ public class main {
     cafe cafe1 = new cafe ("Kenangan", "Indramayu");
     cafe cafe2 = new cafe ("Kopi Tuku", "Bandung");
     cafe cafe3 = new cafe ("Kopi Klotok", "Yogyakarta");
+    cafe3.setNama("")
     
  }
 }
